cc23b0d1550812772620a06c31eef6aa5900d3ca
Muktadir Imam Jan <muktadirimam@gmail.com>
2020-11-08 23:59:49 +0600

    all routes module added

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,10 @@
 import userRoutes from "./user/user.routes";
+import productRoutes from "./product/product.routes";
+import orderRoutes from "./order/order.routes";
 
 export default (app) => {
   app.use("/api/v1/users", userRoutes);
+  app.use("/api/v1/products", productRoutes);
+  app.use("/api/v1/order", orderRoutes);
+  app.use("/api/v1/admin", orderRoutes);
 };
